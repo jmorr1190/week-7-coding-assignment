b0338f88562a583a6fde8f04403ab8fc03309f05
jmorr1190 <jmorr1190@gmail.com>
2023-11-15 06:01:38 -0800

    continued to work through problems

--- week7.js
+++ week7.js
@@ -1,10 +1,8 @@
-/* 1. Create an array called ages that contains the following values: 3, 9, 23, 64, 2, 8, 28, 93.
-
-    - Programmatically subtract the value of the first element in the array from the value in the last element of the array.
-       * Do not use numbers to reference the last element, find it programmatically.
-        ages[7] - ages[0] is not allowed!
-    Add a new age to your array and repeat the step above to ensure it is dynamic. (works for arrays of different lengths).
-    Use a loop to iterate through the array and calculate the average age.*/ 
+// 1. Create an array called ages that contains the following values: 3, 9, 23, 64, 2, 8, 28, 93. - Programmatically subtract the value of the first element in the array from the value in the last element of the array.
+    //     Do not use numbers to reference the last element, find it programmatically.
+    //     ages[7] - ages[0] is not allowed!
+    // Add a new age to your array and repeat the step above to ensure it is dynamic. (works for arrays of different lengths).
+    // Use a loop to iterate through the array and calculate the average age. 
 
 
 let ages = [3, 9, 23, 64, 2, 8, 28, 93]; 
@@ -19,6 +17,18 @@ console.log(ages);
 
     Use a loop to iterate through the array and calculate the average number of letters per name.
     Use a loop to iterate through the array again and concatenate all the names together, separated by spaces. */ 
+
+let names = [ 'Sam', 'Tommy', 'Tim', 'Sally', 'Buck', 'Bob']; 
+
+for (let i = 0; i < names.length; i++) {
+
+    console.log(names[i]);
+}
+
+
+
+
+
 
 
 
