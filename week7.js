910f9ac2b18316fe305ff8105d965b8a7922f7f8
jmorr1190 <jmorr1190@gmail.com>
2023-11-16 05:00:14 -0800

    linked html and js page

--- week7.js
+++ week7.js
@@ -1,22 +1,8 @@
-// 1. Create an array called ages that contains the following values: 3, 9, 23, 64, 2, 8, 28, 93. - Programmatically subtract the value of the first element in the array from the value in the last element of the array.
-    //     Do not use numbers to reference the last element, find it programmatically.
-    //     ages[7] - ages[0] is not allowed!
-    // Add a new age to your array and repeat the step above to ensure it is dynamic. (works for arrays of different lengths).
-    // Use a loop to iterate through the array and calculate the average age. 
+const ages = [3, 9, 23, 64, 2, 8, 28, 93]; 
+   
+const first = age[0];
+console.log(first);
 
-
-let ages = [3, 9, 23, 64, 2, 8, 28, 93]; 
-   let lengths =ages.map(fuction(element) {
-    return element.lengths;
-   }); 
-
-console.log(ages); 
-
-
-/* 1. Create an array called names that contains the following values: 'Sam', 'Tommy', 'Tim', 'Sally', 'Buck', 'Bob'.
-
-    Use a loop to iterate through the array and calculate the average number of letters per name.
-    Use a loop to iterate through the array again and concatenate all the names together, separated by spaces. */ 
 
 let names = [ 'Sam', 'Tommy', 'Tim', 'Sally', 'Buck', 'Bob']; 
 
