69c4dd6392a697931f2aec1adcfad2f308652d09
jmorr1190 <jmorr1190@gmail.com>
2023-11-30 13:44:31 -0800

    work

--- week7.js
+++ week7.js
@@ -1,15 +1,3 @@
-const ages = [3, 9, 23, 64, 2, 8, 28, 93]; 
-   
-const first = age[0];
-console.log(first);
-
-
-let names = [ 'Sam', 'Tommy', 'Tim', 'Sally', 'Buck', 'Bob']; 
-
-for (let i = 0; i < names.length; i++) {
-
-    console.log(names[i]);
-}
 
 
 
