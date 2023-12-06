a54dcfed98b42a363e80f1c4b42c4aaf36b20f18
jmorr1190 <jmorr1190@gmail.com>
2023-12-05 20:26:28 -0800

    worked on q1

--- week7.js
+++ week7.js
@@ -1,3 +1,13 @@
+//*Create an array called ages that contains the following values: 3, 9, 23, 64, 2, 8, 28, 93.
+
+    //* a. Programmatically subtract the value of the first element in the array from the value in the last element of the array.
+       //* - Do not use numbers to reference the last element, find it programmatically.
+        //* - ages[7] - ages[0] is not allowed!
+    //* b. Add a new age to your array and repeat the step above to ensure it is dynamic. (works for arrays of different lengths).
+    //* c. Use a loop to iterate through the array and calculate the average age.
+
+    let ages = (3, 9, 23, 64, 2, 8, 28, 93) => ages(0)-(7); 
+    console.log(ages(0,7)); 
 
 
 
